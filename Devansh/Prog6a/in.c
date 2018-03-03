76c5ecada72adbd962d9b34f95fcb882e07438fb
Devansh <devansh.sharma@gmail.com>
2018-03-03 18:53:44 +0530

    Added comment remover code

---
+++ Devansh/Prog6a/in.c
@@ -0,0 +1,15 @@
+// This is a sample program
+
+#include <stdio.h>	// Include stdio library
+
+void main() {
+	/* A few test cases */
+	/* ,./!@#$%^&*()-=*-+
+	   Random stuff : // Comment in comment
+	*/
+
+	int i;
+
+	printf("// This comment should be in the final code as its not actually comment %d", i);
+}
+
---
+++ Devansh/Prog6a/out.c
@@ -0,0 +1,15 @@
+
+
+#include <stdio.h>	
+
+void main() {
+	
+	
+
+
+
+	int i;
+
+	printf(" This comment should be in the final code as its not actually comment %d", i);
+}
+
